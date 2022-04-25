5ca72959dca1cbc99ea0e8226483966e562bfcaa
Marcos Iorio <marcossiorio@gmail.com>
2022-04-24 21:17:25 -0300

    images fixed?

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    proxy: {
+      "/base": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/base/, ""),
+      },
+    },
+  },
   build: {
     rollupOptions: {
       output: {
